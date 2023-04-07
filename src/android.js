60396b6d3a8ef6ebd8b1cd88358008547ec62241
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-04-07 09:09:51 +0530

    [COMMIT] Initial commit

---
+++ src/android.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Package: device-info
+ * Author: Ganesh B
+ * Description: Nodejs npm module to 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: index.mjs
+ * File Description: 
+ * 
+ * [CONSIDERATION]
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
---
+++ src/mac.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: device-info
+ * Author: Ganesh B
+ * Description: Nodejs npm module to 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: mac.mjs
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
